e70b3892730e471be828c1f801addc5abbd8ed91
Samy <thespecialone68@hotmail.fr>
2019-06-21 14:36:19 +0200

    Ajout AlertDialog au clear de la base de données locale

--- app/src/main/java/com/example/projetcci/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/projetcci/fragments/SettingsFragment.java
@@ -1,8 +1,11 @@
 package com.example.projetcci.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.widget.Toast;
 
 import com.example.projetcci.MovieManager;
 import com.example.projetcci.R;
@@ -21,11 +24,33 @@ public class SettingsFragment extends PreferenceFragment {
         final MovieManager m = new MovieManager(getActivity());
         m.open();
 
+        //AlertDialog creation
+        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage(getString(R.string.clear_ask));
+        builder.setCancelable(true);
+
         //Clear local database
         Preference clearDB = findPreference("key_clear_database");
         clearDB.setOnPreferenceClickListener( new Preference.OnPreferenceClickListener() {
             public boolean onPreferenceClick(Preference pref) {
-                m.clear();
+
+                //Ask user if he really want to clear the local database
+                builder.setPositiveButton(getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        m.clear();
+                        Toast.makeText(getActivity(), getString(R.string.db_cleared), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton(getString(R.string.no), new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+
                 return true;
             }
         });
