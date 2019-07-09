25a8cb4d002021d964f142186aaf729023b78d36
TSO68 <thespecialone68@hotmail.fr>
2019-07-10 00:10:36 +0200

    LoginActivityTest et MainActivityTest (tests d'intégration)

---
+++ app/src/androidTest/java/com/example/projetcci/LoginActivityTest.java
@@ -0,0 +1,40 @@
+package com.example.projetcci;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.widget.TextView;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<LoginActivity> rule = new ActivityTestRule<>(LoginActivity.class);
+
+    @Test
+    public void checkIfTextViewIsPresent() {
+
+        LoginActivity activity = rule.getActivity();
+        TextView tvIntro = activity.findViewById(R.id.titleLogin);
+
+        assertThat(tvIntro, notNullValue());
+        assertThat(tvIntro, instanceOf(TextView.class));
+    }
+
+    @Test
+    public void checkIfTitleIsDisplayed() {
+        LoginActivity activity = rule.getActivity();
+        TextView tvIntro = activity.findViewById(R.id.titleLogin);
+
+        assertThat(tvIntro.getText().toString(), is("Se connecter à ProjetCCI"));
+    }
+
+}
--- app/src/test/java/com/example/projetcci/ValidatorUnitTest.java
+++ app/src/test/java/com/example/projetcci/ValidatorUnitTest.java
@@ -2,7 +2,6 @@ package com.example.projetcci;
 
 import org.junit.Test;
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.*;
 
 public class ValidatorUnitTest {
 
